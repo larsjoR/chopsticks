9a7f9fde8bacf7560e955cd1c41244f9c47fc03f
larsjoR-stb <lars-jo.rosberg@storebrand.no>
2023-10-27 09:22:33 +0200

    game of sharp

---
+++ GameOfLife/C#/ShamoneLife/ShamoneLife/BoardHelper.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace ShamoneLife;
+
+public static class BoardHelper
+{
+
+    public static void SetState(IReadOnlyList<(int x, int y)> allCoordinates, (State State, Rectangle Rect)[,] board)
+    {
+        foreach (var coordinate in allCoordinates)
+        {
+            var state = board[coordinate.x,coordinate.y].State;
+
+            if (state.ShouldBeActive)
+            {
+                SetCoordinateState(coordinate, board);
+                continue;
+            }
+
+            SetCoordinateState(coordinate, board, kill:true);
+        }
+    }
+    public static void UpdateNextState(IReadOnlyList<(int x, int y)> allCoordinates, (State State, Rectangle Rect)[,] board)
+    {
+        /*
+            Any live cell with fewer than two live neighbors dies, as if by underpopulation.
+            Any live cell with two or three live neighbors lives on to the next generation.
+            Any live cell with more than three live neighbors dies, as if by overpopulation.
+            Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+         */
+        foreach (var coordinate in allCoordinates)
+        {
+            var neighborCount = GetNeighborCount(coordinate, board);
+
+            if (board[coordinate.x, coordinate.y].State.IsActive && neighborCount == 2 || neighborCount == 3)
+            {
+                board[coordinate.x, coordinate.y].State.ShouldBeActive = true;
+                continue;
+            }
+
+            if (board[coordinate.x, coordinate.y].State.IsActive &&  neighborCount < 2 || neighborCount > 3)
+            {
+                board[coordinate.x, coordinate.y].State.ShouldBeActive = false;
+            }
+        }
+    }
+    public static int GetNeighborCount((int x, int y) coordinate, (State state, Rectangle rect)[,] board)
+    {
+        var dimension = board.GetLength(0);
+        var surroundingCells = new List<(int x, int y)>();
+        var x = coordinate.x;
+        var y = coordinate.y;
+
+        var north = (x, y - 1);
+        surroundingCells.Add(north);
+        var northEast = (x + 1, y - 1);
+        surroundingCells.Add(northEast);
+        var east = (x + 1, y);
+        surroundingCells.Add(east);
+        var southEast = (x + 1, y + 1);
+        surroundingCells.Add(southEast);
+        var south = (x, y + 1);
+        surroundingCells.Add(south);
+        var southWest = (x - 1, y + 1);
+        surroundingCells.Add(southWest);
+        var west = (x - 1, y);
+        surroundingCells.Add(west);
+        var northWest = (x - 1, y - 1);
+        surroundingCells.Add(northWest);
+
+        var neighborCount = surroundingCells
+            .Where(coordinate => !IsOutOfBounds((coordinate.x, coordinate.y), dimension))
+            .Count(x => board[x.x,x.y].state.IsActive);
+
+        return neighborCount;
+    }
+
+    public static bool IsOutOfBounds((int x, int y) coordinate, int dimension)
+    {
+        var x = coordinate.x;
+        var y = coordinate.y;
+        return x < 0 || x >= dimension || y < 0 || y >= dimension;
+    }
+
+    public static void SetCoordinateState((int x, int y) coordinate, (State State, Rectangle Rect)[,] board, bool kill = false)
+    {
+        if (kill)
+        {
+            board[coordinate.x, coordinate.y].Rect.Fill = Brushes.White;
+            board[coordinate.x, coordinate.y].State.IsActive = false;
+            board[coordinate.x, coordinate.y].State.ShouldBeActive = false;
+            return;
+        }
+
+        board[coordinate.x, coordinate.y].Rect.Fill = Brushes.Black;
+        board[coordinate.x, coordinate.y].State.IsActive = true;
+        
+    }
+
+    public static void UpdateCoordinates(List<(int x,int y)> coordinates, (State, Rectangle)[,] board)
+    {
+        foreach (var coordinate in coordinates)
+        {
+            BoardHelper.SetCoordinateState(coordinate, board);
+        }
+    }
+
+    public static void MakeGliderGun((State, Rectangle)[,] board)
+    {
+        var coordinates = new List<(int x, int y)>
+        {
+            (1, 5), (1, 6), (2, 5), (2, 6), (11, 5), (11, 6), (11, 7),
+            (12, 4), (12, 8), (13, 3), (13, 9), (14, 3), (14, 9), (15, 6),
+            (16, 4), (16, 8), (17, 5), (17, 6), (17, 7), (18, 6),
+            (21, 3), (21, 4), (21, 5), (22, 3), (22, 4), (22, 5),
+            (23, 2), (23, 6), (25, 1), (25, 2), (25, 6), (25, 7),
+            (35, 3), (35, 4), (36, 3), (36, 4)
+        };
+
+        UpdateCoordinates(coordinates, board);
+    }
+
+    public static void MakeSpinner((State, Rectangle)[,] board)
+    {
+        var coordinates = new List<(int x, int y)>
+        {
+            (10, 10), (10, 11), (10, 12)
+        };
+
+        UpdateCoordinates(coordinates, board);
+    }
+
+    public static void MakeGlider((State, Rectangle)[,] board)
+    {
+        var coordinates = new List<(int x, int y)>
+        {
+               (4, 20), (4, 21), (4, 22), (5, 22), (6, 21)
+        };
+
+        UpdateCoordinates(coordinates, board);
+    }
+
+
+    public static void MakePulsar((State, Rectangle)[,] board)
+    {
+
+        var coordinates = new List<(int x, int y)>
+        {
+            (48, 48), (48, 49), (48, 50), (48, 51), (48, 52), (48, 53), (48, 54),
+            (50, 48), (50, 49), (50, 50), (50, 51), (50, 52), (50, 53), (50, 54),
+            (52, 48), (52, 49), (52, 50), (52, 51), (52, 52), (52, 53), (52, 54),
+            (56, 48), (56, 49), (56, 50), (56, 51), (56, 52), (56, 53), (56, 54),
+            (72, 48), (72, 49), (72, 50), (72, 51), (72, 52), (72, 53), (72, 54)
+        };
+
+        UpdateCoordinates(coordinates, board);
+    }
+
+    public static void MakeSimpkinGliderGun((State, Rectangle)[,] board)
+    {
+
+        var coordinates = new List<(int x, int y)>
+        {
+            (2, 12), (2, 13), (3, 12), (3, 13),
+            (7, 12), (7, 13), (7, 14), (8, 11), (8, 15), (9, 10), (9, 16), (10, 10), (10, 16), (11, 11), (11, 15), (12, 12), (12, 13), (12, 14),
+            (13, 11), (13, 15), (14, 10), (14, 16), (15, 10), (15, 16), (16, 11), (16, 15), (17, 12), (17, 13), (17, 14),
+            (22, 12), (22, 13), (23, 12), (23, 13),
+            (25, 12), (25, 13), (25, 14), (26, 11), (26, 15), (27, 10), (27, 16), (28, 10), (28, 16), (29, 11), (29, 15), (30, 12), (30, 13), (30, 14),
+            (31, 11), (31, 15), (32, 10), (32, 16), (33, 10), (33, 16), (34, 11), (34, 15), (35, 12), (35, 13), (35, 14),
+            (42, 12), (42, 13), (43, 12), (43, 13)
+        };
+
+        UpdateCoordinates(coordinates, board);
+    }
+
+    public static void MakePufferTrain((State, Rectangle)[,] board)
+    {
+
+        var coordinates = new List<(int x, int y)>
+        {
+            // Initial puffer
+            (10, 50), (10, 51), (10, 52),
+            (11, 49), (11, 52),
+            (12, 49),
+            (13, 49), (13, 52),
+            (14, 49),
+            (15, 50), (15, 51), (15, 52),
+
+            // Debris trail
+            (22, 49), (22, 50), (22, 51),
+            (23, 48), (23, 51),
+            (24, 48),
+            (25, 48), (25, 51),
+            (26, 48),
+            (27, 49), (27, 50), (27, 51),
+        };
+
+        UpdateCoordinates(coordinates, board);
+    }
+
+    static void MakeRandomBoard((State State, Rectangle Rect)[,] board)
+    {
+        
+    }
+}
---
+++ GameOfLife/C#/ShamoneLife/ShamoneLife/MainWindow.xaml.cs
@@ -0,0 +1,242 @@
+﻿using System.Data.Common;
+using System.Reflection;
+using System.Text;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Timer = System.Threading.Timer;
+
+namespace ShamoneLife;
+
+public partial class MainWindow : Window
+{
+    const int DesiredWidth = 900;
+    const int Dimension = 100;
+    bool ShouldSimulate = true;
+    public Dictionary<(int x, int y), Rectangle> BoardMap { get; set; }
+    public (State State, Rectangle Rect)[,] Board { get; set; }
+    public MainWindow()
+    {
+        BoardMap = new Dictionary<(int x, int y), Rectangle>();
+        Board = new (State, Rectangle)[Dimension, Dimension];
+        InitializeComponent();
+        WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        Width = DesiredWidth;
+        Height = DesiredWidth;
+        Draw(DesiredWidth, Dimension);
+    }
+
+    async void StartSimulation(object sender, EventArgs args)
+    {
+        ShouldSimulate = true;
+
+        while (ShouldSimulate)
+        {
+            BoardHelper.UpdateNextState(BoardMap.Keys.ToList(), Board);
+            BoardHelper.SetState(BoardMap.Keys.ToList(), Board);
+            await Task.Delay(25);
+            InvalidateVisual();
+        }
+    }
+
+    private void DrawCell((int x, int y) coordinate, object sender, EventArgs args)
+    {
+        if (Board[coordinate.x, coordinate.y].State.IsActive)
+        {
+            BoardHelper.SetCoordinateState(coordinate, Board, kill: true);
+            return;
+        }
+
+        BoardHelper.SetCoordinateState(coordinate, Board);
+    }
+
+    void Reset(object sender, EventArgs args)
+    {
+        BoardMap.Keys.ToList()
+            .ForEach(coordinate => BoardHelper.SetCoordinateState(coordinate, Board, kill: true));
+        InvalidateVisual();
+    }
+
+    void StopSimulation(object sender, EventArgs args)
+    {
+        ShouldSimulate = false;
+    }
+    public void Draw(int width, int dimension)
+    {
+        var layout = new StackPanel
+        {
+            Orientation = Orientation.Vertical,
+            HorizontalAlignment = HorizontalAlignment.Center,
+            VerticalAlignment = VerticalAlignment.Center
+        };
+
+        var buttonGrid = new Grid
+        {
+            FlowDirection = FlowDirection.LeftToRight,
+        };
+
+        buttonGrid.RowDefinitions.Add(new RowDefinition());
+        buttonGrid.ColumnDefinitions.Add(new ColumnDefinition());
+        buttonGrid.ColumnDefinitions.Add(new ColumnDefinition());
+        buttonGrid.ColumnDefinitions.Add(new ColumnDefinition());
+        buttonGrid.ColumnDefinitions.Add(new ColumnDefinition());
+
+
+        var startButton = new Button
+        {
+            Content = "Start",
+            Width = 100,
+            Height = 20,
+            Margin = new Thickness(0, 0, 0, 5)
+        };
+
+        var stopButton = new Button
+        {
+            Content = "Stop",
+            Width = 100,
+            Height = 20,
+            Margin = new Thickness(0, 0, 0, 5)
+        };
+
+        var resetButton = new Button
+        {
+            Content = "Reset",
+            Width = 100,
+            Height = 20,
+            Margin = new Thickness(0, 0, 0, 5)
+        };
+
+        startButton.Click += StartSimulation;
+        stopButton.Click += StopSimulation;
+        resetButton.Click += Reset;
+
+        Grid.SetColumn(startButton, 0);
+        Grid.SetColumn(stopButton, 1);
+        Grid.SetColumn(resetButton, 2);
+
+        buttonGrid.Children.Add(startButton);
+        buttonGrid.Children.Add(stopButton);
+        buttonGrid.Children.Add(resetButton);
+
+
+
+        var combobox = new ComboBox
+        {
+            Width = 100,
+            Height = 20,
+            Margin = new Thickness(0, 0, 0, 15)
+        };
+
+        combobox.Items.Add("Glider");
+        combobox.Items.Add("Glider Gun");
+        combobox.Items.Add("Spinner");
+        combobox.Items.Add("Pulsar");
+        combobox.Items.Add("Simpkin Glider Gun");
+        combobox.Items.Add("Puffer Train");
+        combobox.Items.Add("Random");
+        combobox.SelectedIndex = 0;
+
+        Grid.SetColumn(combobox, 3);
+
+        combobox.SelectionChanged += DrawFigure;
+
+        buttonGrid.Children.Add(combobox);
+
+        layout.Children.Add(buttonGrid);
+
+        var grid = new Grid
+        {
+            Width = width * 0.9,
+            Height = width * 0.9,
+            Background = Brushes.White
+        };
+
+        for (var i = 0; i < dimension; i++)
+        {
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.RowDefinitions.Add(new RowDefinition());
+        }
+
+        for (var i = 0; i < dimension; i++)
+        {
+            for (var j = 0; j < dimension; j++)
+            {
+                var rect = new Rectangle
+                {
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 0.1,
+                    Fill = Brushes.White
+                };
+
+                var x = i;
+                var y = j;
+
+                rect.MouseLeftButtonDown += (sender, e) => DrawCell((x, y), sender, e);
+
+                var state = new State
+                {
+                    IsActive = false,
+                    ShouldBeActive = false
+                };
+
+                Grid.SetColumn(rect, i);
+                Grid.SetRow(rect, j);
+
+
+                Board[i, j] = (state, rect);
+                BoardMap.Add((i, j), rect);
+                grid.Children.Add(rect);
+            }
+        }
+
+        layout.Children.Add(grid);
+
+        Content = layout;
+    }
+
+    private void DrawFigure(object sender, SelectionChangedEventArgs e) 
+    {
+        switch (((ComboBox)sender).SelectedItem.ToString())
+        {
+            case "Glider": 
+                BoardHelper.MakeGlider(Board);
+                break;
+
+            case "Glider Gun":
+                BoardHelper.MakeGliderGun(Board);
+                break;
+
+            case "Spinner":
+                BoardHelper.MakeSpinner(Board);
+                break;
+
+            case "Pulsar": 
+                BoardHelper.MakePulsar(Board);
+                break;
+
+            case "Simpkin Glider Gun":
+                BoardHelper.MakeSimpkinGliderGun(Board);
+                break;
+
+            case "Puffer Train":
+                BoardHelper.MakePufferTrain(Board);
+                break;
+
+            default:
+                break;
+        }
+    }
+}
+
+public class State
+{
+    public bool IsActive { get; set; }
+    public bool ShouldBeActive { get; set; }
+}
